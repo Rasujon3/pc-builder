b2d48d6d109f0f1f9042ef6129b3d45c77274f52
Rasujon3 <rasujon3@gmail.com>
2022-02-15 20:36:39 +0600

    my function

--- js/script.js
+++ js/script.js
@@ -1,29 +1,28 @@
-function updatePrice(itemId, price) {
-  const memoryCost = document.getElementById(itemId);
-  memoryCost.innerText = price;
-
-  const bestPrice = document.getElementById("best-price").innerText;
-  const memoryPrice = document.getElementById("memory-cost").innerText;
-  const storagePrice = document.getElementById("storage-cost").innerText;
-  const deliveryPrice = document.getElementById("delivery-cost").innerText;
-
-  const totalPrice =
-    parseFloat(bestPrice) +
-    parseFloat(memoryPrice) +
-    parseFloat(storagePrice) +
-    parseFloat(deliveryPrice);
-
-  const total = document.getElementById("total-price");
-  total.innerText = totalPrice;
-}
-
-function onClick(clickId, updateId, price) {
+function onClickAndUpdatePrice(clickId, itemId, price) {
   document.getElementById(clickId).addEventListener("click", function () {
-    updatePrice(updateId, price);
+    const memoryCost = document.getElementById(itemId);
+    memoryCost.innerText = price;
+
+    const bestPrice = document.getElementById("best-price").innerText;
+    const memoryPrice = document.getElementById("memory-cost").innerText;
+    const storagePrice = document.getElementById("storage-cost").innerText;
+    const deliveryPrice = document.getElementById("delivery-cost").innerText;
+
+    const totalPrice =
+      parseFloat(bestPrice) +
+      parseFloat(memoryPrice) +
+      parseFloat(storagePrice) +
+      parseFloat(deliveryPrice);
+
+    const total = document.getElementById("total-price");
+    total.innerText = totalPrice;
   });
 }
 
-onClick("sixteenGB", "memory-cost", 300);
-onClick("eightGB", "memory-cost", 0);
-onClick("ssd1", "storage-cost", 0);
-onClick("ssd2", "storage-cost", 200);
+onClickAndUpdatePrice("sixteenGB", "memory-cost", 300);
+onClickAndUpdatePrice("eightGB", "memory-cost", 0);
+onClickAndUpdatePrice("ssd1", "storage-cost", 0);
+onClickAndUpdatePrice("ssd2", "storage-cost", 100);
+onClickAndUpdatePrice("ssd3", "storage-cost", 200);
+onClickAndUpdatePrice("free-delivery", "delivery-cost", 0);
+onClickAndUpdatePrice("paid-delivery", "delivery-cost", 20);
